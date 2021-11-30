a279edad77bdbc1bc53671172be2ec122c1b775b
Itta <86896657+itta611@users.noreply.github.com>
2021-11-30 18:12:33 +0900

    fix typo

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
             Bio
           </Caption>
           <ul>
-            <BioSection><BioYear>2008</BioYear>Born in　Aichi, Japan</BioSection>
+            <BioSection><BioYear>2008</BioYear>Born in Aichi, Japan</BioSection>
             <BioSection><BioYear>2020</BioYear>U-22 プログラミング・コンテスト 2020 経済産業省商務政策局長賞 受賞</BioSection>
             <BioSection><BioYear>2021</BioYear>U-22 プログラミング・コンテスト 2021 経済産業大臣賞 受賞</BioSection>
           </ul>
